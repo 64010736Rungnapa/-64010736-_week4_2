5c2608374dd670a0c94700d15e64d637a08bb73b
64010736Rungnapa <88958166+64010736Rungnapa@users.noreply.github.com>
2021-08-29 14:03:55 +0700

    Add files via upload

---
+++ [64010736]_week4_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+  main(){
+      char alphabet ;
+      
+      printf("Alphabet :  ");
+      scanf("%c", &alphabet);
+      printf("Decimal Number : %d",alphabet);
+      
+      return 0;
+  }
